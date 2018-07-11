3b67b821d44ee6341db28c03a5a3dac26bc6127b
kodakodu <kodakodu@gmail.com>
2018-07-11 23:00:42 +0200

    MAJ
    Correction header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { render } from 'react-dom'
-import { Header } from './Layouts/Header'
+import Header from './Layouts/Header'
 
 export default class extends Component {
   render() {
