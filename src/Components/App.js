eb72074eee7bda0cf3aaac7d6478a03011809e3c
kodakodu <kodakodu@gmail.com>
2018-07-11 22:58:38 +0200

    MAJ
    Header et footer

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+import { render } from 'react-dom'
+import { Header } from './Layouts/Header'
+
+export default class extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+      </Fragment>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import { render } from 'react-dom'
+import App from './Components/App'
+import './styles.css'
 
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Hello from CodeSandbox</h2>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+render(<App />, document.getElementById('root'))
