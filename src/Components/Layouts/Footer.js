b473fb145adbfd7ab4abfbb0ff44f64a3feef6f9
kodakodu <kodakodu@gmail.com>
2018-07-14 12:01:10 +0200

    Debut video #2
    https://www.youtube.com/watch?v=HjToX1WWE3w&t

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Tabs, Paper } from 'material-ui'
+import Tab from 'material-ui/Tabs'
 
 export default props => (
-  <div>
-    <h1>Footer</h1>
-  </div>
+  <Paper color="primary">
+    <Tabs value={0} indicatorColor="primary" textColor="primary" centered>
+      <Tab label="Item One" />
+      <Tab label="Item Two" />
+      <Tab label="Item Three" />
+    </Tabs>
+  </Paper>
 )
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
 
 export default props => (
-  <div>
-    <h1>Header</h1>
-  </div>
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="title" color="inherit">
+        Suivi des consommables
+      </Typography>
+    </Toolbar>
+  </AppBar>
 )
