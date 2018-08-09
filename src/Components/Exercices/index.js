ec58a6caf163c644865c55873cbb2865289023e6
kodakodu <kodakodu@gmail.com>
2018-08-09 17:39:08 +0200

    Fin video#2
    Fin video#2
    Pb a voir

--- src/Components/Exercices/index.js
+++ src/Components/Exercices/index.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Grid } from 'material-ui'
+import React from "react";
+import { Grid } from "material-ui";
+import LeftPane from "./LeftPane";
+import RightPane from "./RightPane";
+const styles = {
+  Paper: { padding: 20, marginBottom: 10, marginTop: 10 }
+};
+
 export default props => (
   <Grid container>
-    <Grid item sm>
-      Left pane
-    </Grid>
-    <Grid item sm>
-      Center pane
-    </Grid>
-    <Grid item sm>
-      Right pane
-    </Grid>
+    <Grid item sm={6} />
+    <LeftPane styles={styles} />
+    <Grid item sm={6} />
+    <RightPane styles={styles} />
   </Grid>
-)
+);
